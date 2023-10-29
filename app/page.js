88180c72d4cf23031588ee0edd6b5d8fd1b36397
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-29 10:07:52 -0700

    .

--- app/page.js
+++ app/page.js
@@ -2,14 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <>
-        <Head>
-          <title>For a Good Night, Every Night</title>
-          <link rel="icon" href="sheep-example.ico" /> 
-        </Head>
-        <main style={{ fontFamily: 'Courier, monospace', backgroundColor: '#FFFCF6', height: '100vh', overflow: 'hidden' }} className="flex items-center justify-center">
-          <p style={{ color: '#6D48A2', fontSize: '2rem' }}>hello</p>
-        </main>
-      </>
+    <main style={{ fontFamily: 'Courier, monospace', backgroundColor: '#FFFCF6', height: '100vh', overflow: 'hidden' }} className="flex items-center justify-center">
+      <p style={{ color: '#6D48A2', fontSize: '2rem' }}>hello</p>
+    </main>
   )
 }
