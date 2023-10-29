31466b507b87c299b6bc875e075789ba60287fd6
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-29 12:03:12 -0700

    new

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="favicon.ico" sizes="any" />
-      </Head>
+      <link rel="icon" href="/favicon.ico"/>
       <body className={inter.className}>{children}</body>
     </html>
   )
