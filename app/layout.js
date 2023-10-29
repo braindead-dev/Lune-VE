bb7cde7b772769f8edf751c4049eee0a06d9fef3
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-29 10:11:40 -0700

    new

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Lune VE - For a Good Night, Every Night',
   description: 'Everyone deserves a good night sleep. At Lune, we craft science-backed, health-conscious sleep essentials, uniquely tailored to fit your sleep habits.',
-  
 }
 
 export default function RootLayout({ children }) {
