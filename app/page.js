82ca8052b485003e40c7726e883d5477597359b3
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-28 17:43:17 -0700

    overflow

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main style={{ fontFamily: 'Courier, monospace', backgroundColor: '#FFFCF6' }} className="flex min-h-screen items-center justify-center">
+    <main style={{ fontFamily: 'Courier, monospace', backgroundColor: '#FFFCF6', height: '100vh', overflow: 'hidden' }} className="flex items-center justify-center">
       <p style={{ color: '#6D48A2', fontSize: '2rem' }}>hello</p>
     </main>
   )
