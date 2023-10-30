21219c8bee5de14a4fb8ab68f050032ea637f575
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-29 21:02:05 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-dark-purple">
+      
       <div className="flex-grow flex flex-col justify-center mb-8 relative">
         <div className="absolute inset-0 before:absolute after:absolute after:h-[300px] after:w-[300px] after:translate-x-1/5 after:bg-gradient-conic after:from-yellow-50 after:via-purple-100 after:blur-2xl after:content-[''] z-[-1]"></div>
         <div className="flex items-center font-recoleta text-8xl justify-center z-10 relative">
