7ad8cdc3e0318dae19e4c1888e2d856670c285e3
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-30 16:04:31 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,12 +5,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-dark-purple">
       
       <div className="flex-grow flex flex-col justify-center mb-8 relative">
-        <div className="absolute inset-0 before:absolute after:absolute after:h-[300px] after:w-[300px] after:translate-x-1/5 after:bg-gradient-conic after:from-yellow-50 after:via-purple-100 after:blur-2xl after:content-[''] z-[-1]"></div>
+        <div className="absolute inset-0 before:absolute after:absolute after:h-[300px] after:w-[300px] after:translate-x-1/5 after:bg-gradient-conic after:from-yellow-50 after:via-purple-100 after:blur-2xl after:content-[''] z-[-1] whitespace-nowrap"></div>
         <div className="flex items-center font-recoleta text-8xl justify-center z-10 relative">
           <span className="mr-4">lune</span>
           <Image src="/images/canva-logo.png" alt="Canva Logo" width={100} height={100} />
         </div>
-        <div className="font-recoleta text-2xl">for a good night, every night.</div>
+        <div className="font-recoleta text-2xl whitespace-nowrap">
+            for a good night, every night.
+        </div>
       </div>
       
 
