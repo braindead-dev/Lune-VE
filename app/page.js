95370feb30bb3138951d582ca468d8f7d73407f7
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-10-29 21:01:33 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,17 +4,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-dark-purple">
       <div className="flex-grow flex flex-col justify-center mb-8 relative">
-        <div className="absolute inset-0 before:absolute after:absolute after:h-[300px] after:w-[300px] after:translate-x-1/5 after:bg-gradient-conic after:from-yellow-50 after:via-purple-100 after:blur-2xl after:content-[''] "></div>
+        <div className="absolute inset-0 before:absolute after:absolute after:h-[300px] after:w-[300px] after:translate-x-1/5 after:bg-gradient-conic after:from-yellow-50 after:via-purple-100 after:blur-2xl after:content-[''] z-[-1]"></div>
         <div className="flex items-center font-recoleta text-8xl justify-center z-10 relative">
           <span className="mr-4">lune</span>
           <Image src="/images/canva-logo.png" alt="Canva Logo" width={100} height={100} />
         </div>
       </div>
 
-
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
-
+      <div className="mb-32 grid text-center z-10 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
           href="https://lunesleepsolutions.wixsite.com/lune"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-dark-purple"
