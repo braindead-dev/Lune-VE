09cd724ec724feb3e30c3643e7c06dae570af175
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-11-08 15:02:47 -0800

    removed dark

--- app/components/BackButton.js
+++ app/components/BackButton.js
@@ -3,7 +3,7 @@ function BackButton() {
         <div className="absolute top-8 left-10 z-10">
             <a className="hidden lg:block" href="javascript:history.back()" rel="noopener noreferrer">
                 <button
-                    className="rounded-full border border-gray-300 p-3 flex items-center justify-center bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 font-semibold lg:mt-0 lg:ml-0 lg:static lg:rounded-full lg:bg-gray-200 lg:dark:bg-zinc-800/30"
+                    className="rounded-full border border-gray-300 p-3 flex items-center justify-center bg-gradient-to-b from-zinc-200 backdrop-blur-2xl font-semibold lg:mt-0 lg:ml-0 lg:static lg:rounded-full lg:bg-gray-200"
                     style={{ width: '45px', height: '45px' }}
                 >
                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -1,6 +1,6 @@
 export default function ProgressBar({ percentage }) {
     return (
-      <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mb-4">
+      <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4">
         <div
           className="bg-dark-purple h-2.5 rounded-full transition-width duration-500 ease-in-out"
           style={{ width: `${percentage}%`, transition: 'width 0.5s ease-in-out' }}
