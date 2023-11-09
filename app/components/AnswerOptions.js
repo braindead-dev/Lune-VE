3966367d8343e2a2c0286c3baae0427b20365295
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-11-08 20:21:25 -0800

    formatting

--- app/components/AnswerOptions.js
+++ app/components/AnswerOptions.js
@@ -1,21 +1,17 @@
+import React from 'react';
+
 export default function AnswerOptions({ answers, questionIndex, selectedAnswer, onAnswerChange }) {
-    return (
-      <>
-        {answers.map((answer, index) => (
-          <div key={index} className="text-left">
-            <label>
-              <input 
-                type="radio" 
-                name={`question-${questionIndex}`} 
-                value={answer}
-                onChange={onAnswerChange}
-                checked={selectedAnswer === answer}
-              />
-              {` ${answer}`}
-            </label>
-          </div>
-        ))}
-      </>
-    );
-  }
-  
+  return (
+    <div className="flex flex-col items-center"> 
+      {answers.map((answer, index) => (
+        <div
+          key={index}
+          className={`p-4 my-1 w-full md:max-w-md text-left text-lg rounded-lg border border-gray-300 cursor-pointer ${selectedAnswer === answer ? 'bg-gray-200' : 'bg-transparent'}`}
+          onClick={() => onAnswerChange({ target: { value: answer } }, questionIndex)}
+        >
+          {answer}
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/components/QuestionCard.js
+++ app/components/QuestionCard.js
@@ -2,9 +2,11 @@ import AnswerOptions from './AnswerOptions';
 
 export default function QuestionCard({ question, answers, questionIndex, selectedAnswer, onAnswerChange }) {
   return (
-    <div className="text-center text-2xl mb-3">
-      <p>{`${questionIndex + 1}. ${question}`}</p>
-      <div className="mt-4">
+    <div className="flex flex-col items-center">
+      <div className="w-full md:max-w-xl text-center text-2xl mb-2 px-4">
+        <p>{`${questionIndex + 1}. ${question}`}</p>
+      </div>
+      <div className="w-full md:max-w-md mt-1 mb-2">
         <AnswerOptions 
           answers={answers} 
           questionIndex={questionIndex} 
