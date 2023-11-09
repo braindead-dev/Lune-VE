942793057e2b3bb4c18f5ed2a83a3187a5fe0478
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-11-08 21:57:02 -0800

    Update BackButton.js

--- app/components/BackButton.js
+++ app/components/BackButton.js
@@ -1,3 +1,5 @@
+import { FiArrowLeft } from 'react-icons/fi';
+
 function BackButton() {
     return (
         <div className="absolute top-8 left-10 z-10">
@@ -6,9 +8,7 @@ function BackButton() {
                     className="rounded-full border border-gray-300 p-3 flex items-center justify-center bg-gradient-to-b from-zinc-200 backdrop-blur-2xl font-semibold mt-0 ml-0 static rounded-full bg-gray-200"
                     style={{ width: '45px', height: '45px' }}
                 >
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        {"<"}-
-                    </span>
+                    <FiArrowLeft />
                 </button>
             </a>
         </div>
