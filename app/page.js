ac390ef3a63715d244bef5c7766e9a1afb443a98
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-11-13 08:55:31 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import BackButton from './components/BackButton';
 import GenericCards from './components/GenericCards';
 
 export default function Home() {
   return (
+  
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-dark-purple">
 
       <div className="flex-grow flex flex-col justify-center mb-8 relative">
