e1bd625d82c6f331997f8f9cbc60fc16fe24d5c5
Henry <134236009+braindead-dev@users.noreply.github.com>
2023-11-14 13:38:01 -0800

    Update Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,6 +1,8 @@
+// components/Main.js
+
 export default function Main({ children }) {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-dark-purple overflow-hidden">
+      <main className="flex min-h-screen flex-col justify-between items-center p-24 text-dark-purple">
         {children}
       </main>
     );
